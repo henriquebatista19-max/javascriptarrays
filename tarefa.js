e139d41d51356fe221f83bf4ec3d486267b0e9e6
henriquebatista19-max <henrique.batista19@escola.pr.gov.br>
2026-07-29 15:04:23 +0000

    tarefa.js

--- tarefa.js
+++ tarefa.js
@@ -1,16 +1,25 @@
 const alunos = ['Ana', 'Marcos', 'Maria', 'Mauro'];
 const medias = [7, 4.5, 8, 7.5];
 
+// 1. Reprovados (média menor que 5)
+const reprovados = alunos.filter((aluno, indice) => {
+    return medias[indice] < 5;
+});
+
+console.log('Reprovados:', reprovados);
+
+
+// 2. Aprovados (média maior ou igual a 7)
 const aprovados = alunos.filter((aluno, indice) => {
     return medias[indice] >= 7;
 });
+
 console.log('Aprovados:', aprovados);
 
-const alunos = ['Ana', 'Marcos', 'Maria', 'Mauro'];
-const medias = [7, 4.5, 8, 7.5];
 
+// 3. Recuperação (média entre 5 e 6.9)
 const recuperacao = alunos.filter((aluno, indice) => {
     return medias[indice] >= 5 && medias[indice] < 7;
 });
 
-console.log('Recuperação:', recuperacao)
+console.log('Recuperação:', recuperacao);
